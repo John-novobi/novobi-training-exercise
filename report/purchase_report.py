eb31285c198c5b3edb1133e553655fe427337fae
John-novobi <john.bui@novobi.com>
2022-07-22 07:40:57 +0700

    delete unused code

--- report/purchase_report.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class PurchaseReport(models.Model):
-    _inherit = 'purchase.report'
-
-    payment_term_id = fields.Char(related='order_id.payment_term_id.name', store=True)
