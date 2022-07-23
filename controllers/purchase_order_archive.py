675d46626a342ef935b781c989a2ec4c7f14940a
johnbui-novobi <john.bui@novobi.com>
2022-07-23 12:44:10 +0700

    handle error in controller, restrict ui for specific group

--- controllers/purchase_order_archive.py
+++ controllers/purchase_order_archive.py
@@ -1,25 +1,54 @@
 from odoo import http
 from odoo.http import request, route
-from odoo.exceptions import UserError
+from odoo.exceptions import MissingError, UserError, ValidationError
 
 class PurchaseOrderArchive(http.Controller):
     @route(['/purchase_order/archive'], methods=['POST'], type='json', auth='none')
     def archive_purchase_order(self, **params):
-        method = params.get('method')
-        if method != 'archive':
-            raise UserError('Invalid method !')
-        order_ids = params.get('orders')
-        try:
-            request.env['purchase.order'].sudo().browse(order_ids).action_archive_purchase_order(api_call=True)
+        def _parse_request(params):
+            method = params.get('method')
+            order_ids = params.get('orders')
+            if not method or not order_ids:
+                raise UserError('Invalid parameters')
+            if method != 'archive':
+                raise ValidationError('Invalid method')
+            return {
+                'method': method,
+                'order_ids': order_ids
+            }
+
+        def _get_order_ids(order_ids):
+            found_order_ids = request.env['purchase.order'].sudo().browse(order_ids)
+            if len(found_order_ids) != len(order_ids):
+                raise MissingError('Could not found')
+            else:
+                return found_order_ids
+
+        def _success_response(order_ids):
             return {
                 'archived_orders': order_ids,
                 'code': 200,
                 'message': 'Successful'
             }
-        except:
+
+        def _fail_response(code, message):
             return {
                 'archived_orders': False,
-                'code': 404,
-                'message': 'Could not found'
+                'code': code,
+                'message': message
             }
-            
+        
+        response = {}
+        try:
+            request_data = _parse_request(params)
+            _get_order_ids(request_data['order_ids']).action_archive_purchase_order(api_call=True)
+            response = _success_response(request_data['order_ids'])
+        except ValidationError as e:
+            response = _fail_response(code=422, message=e)
+        except MissingError as e:
+            response = _fail_response(code=404, message=e)
+        except UserError as e:
+            response = _fail_response(code=400, message=e)
+        except:
+            response = _fail_response(code=500, message='Internal server error')
+        return response
--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -4,3 +4,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     archive_lifespan = fields.Integer(string='Archive lifespan (minutes)', config_parameter='purchase_order_archive_lifespan', default=30)
+    
